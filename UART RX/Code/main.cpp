3676ee31157721aaa58fb5d045452720ee7da08f
Feyfa <fisikamodern00@gmail.com>
2022-12-25 11:16:24 +0700

    project

--- UART RX/Code/main.cpp
+++ UART RX/Code/main.cpp
@@ -11,14 +11,14 @@
 
 int main(void)
 {
-	DDRD |= (1<<2)|(1<<3);
-	uart uart1 = uart(_8MHZ,9600);
+	DDRD |= (1<<2)|(1<<3);//set PD2,PD3 as output
+	uart uart1 = uart(_8MHZ,9600);//using F_CPU 8MHZ and baud rate 9600
 	
     while (1) 
     {
-		switch(uart1.getReceive()){
-			case 'a': PORTD = (1<<2); break;
-			case 'b': PORTD = (1<<3); break;	
+		switch(uart1.getReceive()){//take the result of receive uart
+			case 'a': PORTD = (1<<2); break;//if 'a' PD2 is HIGH
+			case 'b': PORTD = (1<<3); break;//if 'b' PD3 is HIGH
 		}
     }
 }
--- UART TX/Code/main.cpp
+++ UART TX/Code/main.cpp
@@ -11,19 +11,19 @@
 
 int main(void)
 {
-	DDRD &= ~((1<<2)|(1<<3));
-	PORTD |= (1<<2)|(1<<3);
-	uart uart1 = uart(_8MHZ,9600);
+	DDRD &= ~((1<<2)|(1<<3));//set PD2,PD3 as input
+	PORTD |= (1<<2)|(1<<3);//activate pull ups in PD2,PD3
+	uart uart1 = uart(_8MHZ,9600);//using F_CPU 8MHZ and baud rate 9600
 	
     while (1) 
     {
-		if(bit_is_clear(PIND,2)){
-			uart1.setTransmitter('a');
-			_delay_ms(500);	
+		if(bit_is_clear(PIND,2)){//if PIND in PD2 clear
+			uart1.setTransmitter('a');//send character 'a'
+			_delay_ms(500);//pause 500 milliseconds
 		}
-		if(bit_is_clear(PIND,3)){
-			uart1.setString(" hello ");
-			_delay_ms(500);		
+		if(bit_is_clear(PIND,3)){//if PIND in PD3 clear
+			uart1.setString(" hello ");//send the string "hello"
+			_delay_ms(500);//pause 500 milliseconds		
 		}
     }
 }
